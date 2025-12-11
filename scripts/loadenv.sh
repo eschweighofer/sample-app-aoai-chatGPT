df5986bd9229c87c39e9ace3eb40890e86c227bb
Erik Schweighofer <eschweighofer@gmail.com>
2025-12-11 00:54:40 +0000

    Add virtual environment setup and dependency installation to start script Refactor loadenv scripts fix: update installation message to reflect correct requirements file

--- scripts/loadenv.sh
+++ scripts/loadenv.sh
@@ -14,10 +14,3 @@ $(azd env get-values --no-prompt)
 EOF
     fi
 fi
-
-
-echo 'Creating Python virtual environment ".venv" in root'
-python3 -m venv .venv
-
-echo 'Installing dependencies from "requirements.txt" into virtual environment'
-./.venv/bin/python -m pip install -r requirements-dev.txt
--- start.cmd
+++ start.cmd
@@ -1,15 +1,6 @@
 @echo off
 
 echo.
-echo Restoring backend python packages
-echo.
-call python -m pip install -r requirements.txt
-if "%errorlevel%" neq "0" (
-    echo Failed to restore backend python packages
-    exit /B %errorlevel%
-)
-
-echo.
 echo Restoring frontend npm packages
 echo.
 cd frontend
@@ -28,8 +19,33 @@ if "%errorlevel%" neq "0" (
     exit /B %errorlevel%
 )
 
+cd ..
+call ./scripts/loadenv.ps1
+
+echo.
+echo Restoring backend python packages from requirements-dev.txt
+echo.
+
+$pythonCmd = Get-Command python -ErrorAction SilentlyContinue
+if (-not $pythonCmd) {
+  # fallback to python3 if python not found
+  $pythonCmd = Get-Command python3 -ErrorAction SilentlyContinue
+}
+
+Write-Host 'Creating Python virtual environment ".venv" in root'
+Start-Process -FilePath ($pythonCmd).Source -ArgumentList "-m venv ./.venv" -Wait -NoNewWindow
+
+$venvPythonPath = "./.venv/scripts/python.exe"
+if (Test-Path -Path "/usr") {
+  # fallback to Linux venv path
+  $venvPythonPath = "./.venv/bin/python"
+}
+
+Write-Host 'Installing dependencies from "requirements-dev.txt" into virtual environment'
+Start-Process -FilePath $venvPythonPath -ArgumentList "-m pip install -r ./requirements-dev.txt" -Wait -NoNewWindow
+
 echo.    
-echo Starting backend    
+echo Starting backend
 echo.    
 cd ..  
 start http://127.0.0.1:50505
